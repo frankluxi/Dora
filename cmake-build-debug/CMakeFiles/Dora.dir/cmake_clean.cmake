7ee07b5770ff50581208f89b5e7b8e58aff090a1
frankluxi <frank_lu@163.com>
2020-04-09 15:02:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Dora.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dora.dir/Frontend/BaseSource.cpp.o"
+  "CMakeFiles/Dora.dir/Frontend/FileSource.cpp.o"
+  "CMakeFiles/Dora.dir/Frontend/Parser.cpp.o"
+  "CMakeFiles/Dora.dir/Frontend/Scaner.cpp.o"
+  "CMakeFiles/Dora.dir/Frontend/Token.cpp.o"
+  "CMakeFiles/Dora.dir/Frontend/TokenType.cpp.o"
+  "CMakeFiles/Dora.dir/Test/TestIO.cpp.o"
+  "CMakeFiles/Dora.dir/Test/TestMessage/CompilerTester.cpp.o"
+  "CMakeFiles/Dora.dir/Test/TestMessage/TestMessageListener.cpp.o"
+  "CMakeFiles/Dora.dir/main.cpp.o"
+  "Dora"
+  "Dora.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
